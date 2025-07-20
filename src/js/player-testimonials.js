b656c8eb9d1bc7c0dcf0e018c0d2d1d05b79fd68
Woolf <woolfson1995@gmail.com>
2025-07-20 18:03:20 +0300

    Update player-testimonials.js

--- src/js/player-testimonials.js
+++ src/js/player-testimonials.js
@@ -26,6 +26,6 @@ const swiperProjects = new Swiper('[data-swiper="player"]', {
     slideChange: updateNavState,
   },
   breakpoints: {
-    1200: {  slidesPerView: 'auto', spaceBetween: 24 },
+    1200: { slidesPerView: 3, spaceBetween: 23 },
   },
 });
