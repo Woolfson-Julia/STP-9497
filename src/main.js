01667598a798f640459b4a4d7af177474d790411
Tetyana Solyanyk <tanyas81@gmail.com>
2025-07-24 14:47:04 +0300

    Refactor CSS for responsive design and remove unused footer JS

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/menu-modal';
 import './js/player-testimonials';
 import './js/faqAccordion';
-import './js/footer';
 import './js/cookies-modal';
 import './js/game-info';
 import './js/header';
